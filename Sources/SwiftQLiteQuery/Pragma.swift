0f61fe392315a27c2f2f21d25ff4e2f065c4e54d
Braden Scothern <BradenScothern@gmail.com>
2020-10-16 23:12:30 -0600

    More progress on Pragma

--- Sources/SwiftQLiteQuery/Pragma.swift
+++ Sources/SwiftQLiteQuery/Pragma.swift
@@ -68,12 +68,22 @@ public struct Pragma: Statement {
         self.directive = directive
         self.value = value
     }
+
+    @usableFromInline
+    init<T>(schema: SchemaName? = nil, directive: String, equals value: T?) where T: LosslessStringConvertible {
+        self.init(schema: schema, directive: directive, value: .equals(value))
+    }
+
+    @usableFromInline
+    init<T>(schema: SchemaName? = nil, directive: String, parenthesis value: T?) where T: LosslessStringConvertible {
+        self.init(schema: schema, directive: directive, value: .parenthesis(value))
+    }
 }
 
 extension Pragma {
     @inlinable
     public static func analysisLimit(_ limit: Int32? = nil) -> Self {
-        .init(directive: "analysis_limit", value: .equals(limit))
+        .init(directive: "analysis_limit", equals: limit)
     }
 
     @inlinable
@@ -81,7 +91,7 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "application_id",
-            value: .equals(id)
+            equals: id
         )
     }
 
@@ -104,7 +114,7 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "auto_vacuum",
-            value: .equals(mode.rawValue)
+            equals: mode.rawValue
         )
     }
 
@@ -112,7 +122,7 @@ extension Pragma {
     public static func automaticIndex(_ value: Bool? = nil) -> Self {
         .init(
             directive: "automatic_index",
-            value: .equals(value)
+            equals: value
         )
     }
 
@@ -120,7 +130,7 @@ extension Pragma {
     public static func busyTimeout(milliseconds: Int32? = nil) -> Self {
         .init(
             directive: "busy_timeout",
-            value: .equals(milliseconds)
+            equals: milliseconds
         )
     }
 
@@ -134,7 +144,7 @@ extension Pragma {
     public static func cacheSpill(enabled: Bool? = nil) -> Self {
         .init(
             directive: "cache_spill",
-            value: .equals(enabled)
+            equals: enabled
         )
     }
 
@@ -143,7 +153,7 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "cache_spill",
-            value: .equals(threshold)
+            equals: threshold
         )
     }
 
@@ -151,7 +161,7 @@ extension Pragma {
     public static func caseSensitiveLike(enabled: Bool) -> Self {
         .init(
             directive: "case_sensitive_like",
-            value: .equals(enabled)
+            equals: enabled
         )
     }
 
@@ -159,7 +169,7 @@ extension Pragma {
     public static func cellSizeCheck(enabled: Bool? = nil) -> Self {
         .init(
             directive: "cell_size_check",
-            value: .equals(enabled)
+            equals: enabled
         )
     }
 
@@ -167,7 +177,7 @@ extension Pragma {
     public static func checkpointFullFsync(enabled: Bool? = nil) -> Self {
         .init(
             directive: "checkpoint_fullfsync",
-            value: .equals(enabled)
+            equals: enabled
         )
     }
 
@@ -186,7 +196,7 @@ extension Pragma {
     public static func countChanges(enabled: Bool? = nil) -> Self {
         .init(
             directive: "count_changes",
-            value: .equals(enabled)
+            equals: enabled
         )
     }
 
@@ -195,7 +205,7 @@ extension Pragma {
     public static func dataStore(directory: String? = nil) -> Self {
         .init(
             directive: "data_store_directory",
-            value: .equals(directory.map { "'\($0)'" })
+            equals: directory.map { "'\($0)'" }
         )
     }
 
@@ -218,7 +228,7 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "default_cache_size",
-            value: .equals(numberOfPages)
+            equals: numberOfPages
         )
     }
 
@@ -226,7 +236,7 @@ extension Pragma {
     public static func deferForeignKeys(enabled: Bool? = nil) -> Self {
         .init(
             directive: "defer_foreign_keys",
-            value: .equals(enabled)
+            equals: enabled
         )
     }
 
@@ -235,7 +245,7 @@ extension Pragma {
     public static func emptyResultCallbacks(enabled: Bool? = nil) -> Self {
         .init(
             directive: "empty_result_callbacks",
-            value: .equals(enabled)
+            equals: enabled
         )
     }
 
@@ -250,7 +260,7 @@ extension Pragma {
     public static func encoding(_ value: Encoding? = nil) -> Self {
         .init(
             directive: "encoding",
-            value: .equals(value?.rawValue)
+            equals: value?.rawValue
         )
     }
 
@@ -259,7 +269,7 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "foreign_key_check",
-            value: .parenthesis(table?.substatementValue)
+            parenthesis: table?.substatementValue
         )
     }
 
@@ -267,7 +277,7 @@ extension Pragma {
     public static func foreignKeyList(table: TableName) -> Self {
         .init(
             directive: "foreign_key_list",
-            value: .parenthesis(table.substatementValue)
+            parenthesis: table.substatementValue
         )
     }
 
@@ -275,7 +285,7 @@ extension Pragma {
     public static func foreignKeys(enabled: Bool? = nil) -> Self {
         .init(
             directive: "foreign_keys",
-            value: .equals(enabled)
+            equals: enabled
         )
     }
 
@@ -292,7 +302,7 @@ extension Pragma {
     public static func fullColumnNames(enabled: Bool? = nil) -> Self {
         .init(
             directive: "full_column_names",
-            value: .equals(enabled)
+            equals: enabled
         )
     }
 
@@ -300,7 +310,7 @@ extension Pragma {
     public static func fullFSync(enabled: Bool? = nil) -> Self {
         .init(
             directive: "fullfsync",
-            value: .equals(enabled)
+            equals: enabled
         )
     }
 
@@ -310,18 +320,18 @@ extension Pragma {
     }
 
     @inlinable
-    public static func hardHeapLimit(_ value: Int64? = nil) -> Self {
+    public static func hardHeapLimit(_ limit: Int64? = nil) -> Self {
         .init(
             directive: "hard_heap_limit",
-            value: .equals(value)
+            equals: limit
         )
     }
 
     @inlinable
-    public static func ignoreCheckConstraints(_ value: Bool) -> Self {
+    public static func ignoreCheckConstraints(_ enabled: Bool) -> Self {
         .init(
             directive: "ignore_check_constraints",
-            value: .equals(value)
+            equals: enabled
         )
     }
 
@@ -330,7 +340,7 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "incremental_vacuum",
-            value: .parenthesis(numberOfPages)
+            parenthesis: numberOfPages
         )
     }
 
@@ -339,7 +349,7 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "index_info",
-            value: .parenthesis(index.substatementValue)
+            parenthesis: index.substatementValue
         )
     }
 
@@ -348,7 +358,7 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "index_list",
-            value: .parenthesis(table.substatementValue)
+            parenthesis: table.substatementValue
         )
     }
 
@@ -357,7 +367,7 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "index_xinfo",
-            value: .parenthesis(index.substatementValue)
+            parenthesis: index.substatementValue
         )
     }
 
@@ -374,7 +384,7 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "integrity_check",
-            value: .parenthesis(maximumNumberOfErrors)
+            parenthesis: maximumNumberOfErrors
         )
     }
 
@@ -383,7 +393,185 @@ extension Pragma {
         .init(
             schema: schema,
             directive: "integrity_check",
-            value: .parenthesis(table.substatementValue)
+            parenthesis: table.substatementValue
+        )
+    }
+
+    public enum JournalMode: String {
+        case delete = "DELETE"
+        case truncate = "TRUNCATE"
+        case persist = "PERSIST"
+        case memory = "MEMORY"
+        case wal = "WAL"
+        case off = "OFF"
+    }
+
+    @inlinable
+    public static func journalMode(schema: SchemaName, mode: JournalMode? = nil) -> Self {
+        .init(
+            schema: schema,
+            directive: "journal_mode",
+            equals: mode?.rawValue
+        )
+    }
+
+    @inlinable
+    public static func journalSizeLimit(_ limit: Int64? = nil) -> Self {
+        .init(
+            directive: "journal_size_limit",
+            equals: limit
+        )
+    }
+
+    @inlinable
+    public static func legacyAlterTitle(_ enabled: Bool? = nil) -> Self {
+        .init(
+            directive: "legacy_alter_table",
+            equals: enabled
+        )
+    }
+
+    @inlinable
+    public static func legacyFileFormat() -> Self {
+        .init(directive: "legacy_file_format")
+    }
+
+    public enum LockingMode: String {
+        case normal = "NORMAL"
+        case exclusive = "EXCLUSIVE"
+    }
+
+    @inlinable
+    public static func lockingMode(schema: SchemaName, mode: LockingMode? = nil) -> Self {
+        .init(
+            schema: schema,
+            directive: "locking_mode",
+            equals: mode?.rawValue
+        )
+    }
+
+    @inlinable
+    public static func maxPageCount(schema: SchemaName, count: Int64? = nil) -> Self {
+        .init(
+            schema: schema,
+            directive: "max_page_count",
+            equals: count
+        )
+    }
+
+    @inlinable
+    public static func mmapSize(schema: SchemaName, size: Int64? = nil) -> Self {
+        .init(
+            schema: schema,
+            directive: "mmap_size",
+            equals: size
+        )
+    }
+
+    @inlinable
+    public static func moduleList() -> Self {
+        .init(directive: "module_list")
+    }
+
+    public struct OptimizeMask: OptionSet {
+        public typealias RawValue = Int
+
+        public static let debug = Self(rawValue: 1)
+        public static let analyze = Self(rawValue: 2)
+
+        @available(*, deprecated, message: "Not yet implemented by SQLite")
+        public static let record = Self(rawValue: 4)
+
+        @available(*, deprecated, message: "Not yet implemented by SQLite")
+        public static let createHelpfulIndexes = Self(rawValue: 8)
+
+        public let rawValue: RawValue
+
+        @inlinable
+        public init(rawValue: RawValue) {
+            self.rawValue = rawValue
+        }
+    }
+
+    @inlinable
+    public static func optimize(schema: SchemaName? = nil, mask: OptimizeMask? = nil) -> Self {
+        .init(
+            schema: schema,
+            directive: "optimize",
+            parenthesis: mask?.rawValue
+        )
+    }
+
+    @inlinable
+    public static func pageCount(schema: SchemaName) -> Self {
+        .init(
+            schema: schema,
+            directive: "page_count"
+        )
+    }
+
+    @inlinable
+    public static func pageSize(schema: SchemaName, bytes: Int) -> Self {
+        .init(
+            schema: schema,
+            directive: "page_size",
+            equals: bytes
+        )
+    }
+
+    @inlinable
+    @available(*, deprecated, message: "This pragma is intended for use when debugging SQLite itself. It is only available when the SQLITE_DEBUG compile-time option is used.")
+    public static func parserTrace(enabled: Bool) -> Self {
+        .init(
+            directive: "parser_trace",
+            equals: enabled
+        )
+    }
+
+    @inlinable
+    public static func pragmaList() -> Self {
+        .init(directive: "pragma_list")
+    }
+
+    @inlinable
+    public static func queryOnly(enabled: Bool? = nil) -> Self {
+        .init(
+            directive: "query_only",
+            equals: enabled
+        )
+    }
+
+    @inlinable
+    public static func quickCheck(schema: SchemaName) -> Self {
+        .init(
+            schema: schema,
+            directive: "quick_check"
+        )
+    }
+
+    @inlinable
+    public static func quickCheck(schema: SchemaName, maximumNumberOfErrors: Int64) -> Self {
+        .init(
+            schema: schema,
+            directive: "quick_check",
+            parenthesis: maximumNumberOfErrors
+        )
+    }
+
+    @inlinable
+    public static func quickCheck(schema: SchemaName, table: TableName) -> Self {
+        .init(
+            schema: schema,
+            directive: "quick_check",
+            parenthesis: table.substatementValue
+        )
+    }
+
+    @inlinable
+    public static func readUncommited(enabled: Bool? = nil) -> Self {
+        .init(
+            directive: "read_uncommited",
+            equals: enabled
         )
     }
 }
